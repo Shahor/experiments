57b6591db72806c6d3e1bfb495341d935ccaf844
Shahor <gaudencio42@gmail.com>
2011-03-29 00:05:14 +0200

    bouncingBall
    	* It does bounce !

--- html5/bouncingBall/main.js
+++ html5/bouncingBall/main.js
@@ -1,32 +1,31 @@
 $(function (){
+	var xStep = 10;
+	var yStep = 4;
 	var canvas = document.getElementById('sandbox');
 	var ctx = canvas.getContext('2d');
 	var img = new Image();
 	var animationTimer;
-	var position = {
+	var ballParams = {
+//		'didHitBorder' : false,
 		'x' : 0,
-		'y' : 0
+		'xDirection': 1,
+		'y' : 0,
+		'yDirection' : 1
 	};
 
 	img.onload = function() {
 		animationTimer = setInterval(function () {
-			console.log("Image positions  x : " + position.x + " | y : " + position.y);
-			if (position.x + 80 == canvas.width)
-			{
-					clearInterval(animationTimer);
-					return false;
-			}
-			else
-			{
-				position.x += 5;
-				position.y += 5;
-				ctx.clearRect(0, 0, canvas.width, canvas.height);
-				ctx.drawImage(img, position.x, position.y);
-			}
+			console.log("Image positions  x : " + ballParams.x + " | y : " + ballParams.y);
+			ballParams.x = ballParams.xDirection > 0 ? ballParams.x + xStep : ballParams.x - xStep;
+			ballParams.y = ballParams.yDirection > 0 ? ballParams.y + yStep : ballParams.y - yStep;
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			ctx.drawImage(img, ballParams.x, ballParams.y);
 			
-		}, 100)
-
-		
+			if (ballParams.x + 80 === canvas.width || ballParams.x === 0)
+				ballParams.xDirection = -ballParams.xDirection;
+			if (ballParams.y + 80 === canvas.height || ballParams.y === 0)
+				ballParams.yDirection = -ballParams.yDirection;
+		}, 10);
 	};
 	img.src = 'soccer.jpg';
 });
