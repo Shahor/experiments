a77b305c9dc162e3bb0a63c03f1e505cce9bf609
Shahor <gaudencio42@gmail.com>
2011-03-29 00:49:23 +0200

    bouncing Ball
    	* Bounce effect

--- html5/bouncingBall/main.js
+++ html5/bouncingBall/main.js
@@ -1,28 +1,46 @@
 window.onload = function () {
-	var xStep = 7;
-	var yStep = 4;
 	var canvas = document.getElementById('sandbox');
 	var ctx = canvas.getContext('2d');
 	var img = new Image();
+	var appParams = {
+		'flatCoef' : 8,
+		'xStep' : 7,
+		'yStep' : 4
+	};
 	var ballParams = {
 		'x' : 0,
-		'xDirection': 1,
 		'y' : 0,
-		'yDirection' : 1
+		'xDirection': 1,
+		'yDirection' : 1,
+		'flatEffect' : {
+			'y' : 0,
+			'x' : 0
+			}
 	};
 
 	img.onload = function() {
 		setInterval(function () {
-			ballParams.x = ballParams.xDirection > 0 ? ballParams.x + xStep : ballParams.x - xStep;
-			ballParams.y = ballParams.yDirection > 0 ? ballParams.y + yStep : ballParams.y - yStep;
+			if (ballParams.flatEffect.x > 0)
+				ballParams.flatEffect.x -= appParams.flatCoef / 4;
+			if (ballParams.flatEffect.y > 0)
+				ballParams.flatEffect.y -= appParams.flatCoef / 4;
+			
+			ballParams.x = ballParams.xDirection > 0 ? ballParams.x + appParams.xStep : ballParams.x - appParams.xStep;
+			ballParams.y = ballParams.yDirection > 0 ? ballParams.y + appParams.yStep : ballParams.y - appParams.yStep;
 			
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			ctx.drawImage(img, ballParams.x, ballParams.y);
+			ctx.drawImage(img, ballParams.x, ballParams.y, img.width - ballParams.flatEffect.x, img.height - ballParams.flatEffect.y);
 			
 			if (ballParams.x + img.width >= canvas.width || ballParams.x === 0)
+			{
+				if (ballParams.flatEffect.x == 0) ballParams.flatEffect.x = 3 * appParams.flatCoef;
 				ballParams.xDirection = -ballParams.xDirection;
+			}
 			if (ballParams.y + img.height >= canvas.height || ballParams.y === 0)
+			{
+				if (ballParams.flatEffect.y == 0) ballParams.flatEffect.y = 3 * appParams.flatCoef;
 				ballParams.yDirection = -ballParams.yDirection;
+			}
 		}, 20);
 	};
 	img.src = 'soccer.jpg';
