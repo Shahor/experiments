bf1007e25ba80698bc457e3423236f0f3a64f4c7
Shahor <gaudencio42@gmail.com>
2011-03-29 00:27:20 +0200

    bouncingBall
    	* Removed useless code
    	* Removed jQuery

--- html5/bouncingBall/main.js
+++ html5/bouncingBall/main.js
@@ -1,10 +1,9 @@
-$(function (){
+window.onload = function () {
 	var xStep = 7;
 	var yStep = 4;
 	var canvas = document.getElementById('sandbox');
 	var ctx = canvas.getContext('2d');
 	var img = new Image();
-	var animationTimer;
 	var ballParams = {
 		'x' : 0,
 		'xDirection': 1,
@@ -13,17 +12,18 @@ $(function (){
 	};
 
 	img.onload = function() {
-		animationTimer = setInterval(function () {
+		setInterval(function () {
 			ballParams.x = ballParams.xDirection > 0 ? ballParams.x + xStep : ballParams.x - xStep;
 			ballParams.y = ballParams.yDirection > 0 ? ballParams.y + yStep : ballParams.y - yStep;
+			
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
 			ctx.drawImage(img, ballParams.x, ballParams.y);
 			
-			if (ballParams.x + 80 >= canvas.width || ballParams.x === 0)
+			if (ballParams.x + img.width >= canvas.width || ballParams.x === 0)
 				ballParams.xDirection = -ballParams.xDirection;
-			if (ballParams.y + 80 >= canvas.height || ballParams.y === 0)
+			if (ballParams.y + img.height >= canvas.height || ballParams.y === 0)
 				ballParams.yDirection = -ballParams.yDirection;
 		}, 10);
 	};
 	img.src = 'soccer.jpg';
-});
+};
